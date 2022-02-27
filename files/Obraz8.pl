2a500c0067b96e802225839defd0ce1174acce9f
odyszy <74557267+odyszy@users.noreply.github.com>
2022-02-27 18:36:35 +0100

    Add files via upload

---
+++ files/Obraz8.pl
@@ -0,0 +1,16 @@
+character(chandler).
+character(rachel).
+character(ross).
+character(monica).
+character(joey).
+character(phoebe).
+
+loves(rachel, ross).
+loves(rachel, joey).
+loves(monica, chandler).
+loves(ross, rachel).
+loves(joey, rachel).
+loves(chandler, monica).
+
+eatLeftDessert(chandler; rachel; ross).
+eatRightDessert(monica; joey; phoebe).
